730429a4b7d178af916cb586be7b494bc224e452
jojo-koomson <koomson.lm@gmail.com>
2024-05-27 11:37:39 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import SectionView from './container/SectionView/SectionView';
+
+
+function App() {
+  return (
+    <>
+        <Navbar/>
+        <SectionView/>
+
+    </>
+  )
+}
+
+export default App;
